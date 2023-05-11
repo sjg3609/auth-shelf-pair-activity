a9495b4a7e58a4bb33d0b4f82bfbac269e43c19c
Sam Gossie <sjg3609@gmail.com>
2023-05-11 10:15:25 -0500

    Completed forms to add a new item

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+ } = require('../modules/authentication-middleware');
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   let queryText = `SELECT * FROM "item"`;
   pool.query(queryText).then((result) => {
     res.send(result.rows);
@@ -18,15 +21,25 @@ router.get('/', (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const item = req.body
   // endpoint functionality
+  let queryText = `INSERT INTO "item" ("description", "image_url", "user_id")
+                   VALUES ($1, $2, $3);`;
+  pool.query(queryText, [item.name, item.image, req.user.id]).then((result) => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log(`Error in POST ${error}`);
+    res.sendStatus(501);
+  })
 });
 
 /**
  * Delete an item
  */
-router.delete('/:id', (req, res) => {
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
   // endpoint functionality
+  let queryText = `DELETE FROM "item" WHERE id=$1`
 });
 
 module.exports = router;
--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import './ShelfPage.css';
 
 function ShelfPage() {
   const [shelfList, setShelfList] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     fetchShelf();
@@ -17,11 +19,33 @@ function ShelfPage() {
       alert('Something went wrong.');
     });
   }
+  
+  // const handleChange = (event) => {
+  //   event.preventDefault();
+  //   const action = { name: event.target.value};
+  //   dispatch(action);
+  // }
+
+  const addShelf = (event) => {
+    event.preventDefault();
+    const newItem = { name: event.target.firstElementChild.value, image: event.target.children[1].value }
+    axios.post('/api/shelf', newItem).then((response) => {
+      fetchShelf();
+    }).catch((error) => {
+      console.log(error);
+      alert('Something went wrong.');
+    });
+  }
 
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <form onSubmit={addShelf}>
+        <input type='text' placeholder="Name" />
+        <input type='text' placeholder='Image' />
+        <input type='submit' />
+      </form>
       {
         shelfList.length === 0 && (
           <div>No items on the shelf</div>
